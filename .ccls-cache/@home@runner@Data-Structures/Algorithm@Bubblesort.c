9eca8c978ebaafe8f641d8833c2800389318fae9
dkvvvarma <152471024+dkvvvarma@users.noreply.github.com>
2024-01-14 14:15:56 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Data-Structures/Algorithm@Bubblesort.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+  int i=0, n=7,j=0;
+  int size =sizeof(arr)/sizeof(arr[0]);
+  for(i=0;i<n-1;i++)
+    {
+      for(j=0,j<n-1;j++)
+        {
+          if(arr[j]>arr[j+1])
+          {
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+          }
+        }
+    }
+  {
+    for(i=0;i<size;i++)
+      printf("%d,",arr[i]);  
+  }
+  return(0);
+}
